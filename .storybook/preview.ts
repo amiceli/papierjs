f45ec1933241f7326cfa58b83e684a84b0ef7f39
amiceli <amiceli@letsignit.com>
2025-02-08 11:38:04 +0100

    chore: add biome and storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from '@storybook/html'
+import { defineCustomElements } from '../loader'
+
+defineCustomElements()
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+}
+
+export default preview
