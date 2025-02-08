ccaa456efc6ad4f8a26305f2c8540dde29a409f0
amiceli <amiceli@letsignit.com>
2025-02-08 19:21:57 +0100

    feat: add progress bar component

---
+++ src/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,72 @@
+// biome-ignore lint/correctness/noUnusedImports: required for jsx
+import { Component, Prop, h } from '@stencil/core'
+
+type ProgressBarColor =
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | 'muted'
+    | 'primary'
+
+@Component({
+    tag: 'p-progress-bar',
+    styleUrl: 'progress-bar.scss',
+    shadow: true,
+})
+export class ProgressBar {
+    @Prop()
+    type?: ProgressBarColor = 'primary'
+    @Prop()
+    value?: number = 0
+    @Prop()
+    striped?: boolean = false
+    @Prop()
+    dark?: boolean = false
+
+    public getClass(): string {
+        const types = [
+            'secondary',
+            'success',
+            'warning',
+            'danger',
+            'muted',
+            'primary',
+        ]
+        let cssClass = `bar w-${this.value}`
+
+        if (!types.includes(this.type)) {
+            cssClass = `${cssClass} primary`
+        } else {
+            cssClass = `${cssClass} ${this.type}`
+        }
+
+        if (this.striped) {
+            cssClass = `${cssClass} striped`
+        }
+
+        return cssClass
+    }
+
+    public getParentClass() {
+        let cssClass = 'papier is--block'
+
+        if (this.dark) {
+            cssClass = `${cssClass} is--dark`
+        }
+
+        return cssClass
+    }
+
+    render() {
+        return (
+            <div class={this.getParentClass()}>
+                <div class="progress">
+                    <div class={this.getClass()}>
+                        <slot />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/progress-bar/readme.md
@@ -0,0 +1,20 @@
+# p-progress-bar
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property  | Attribute | Description | Type                                                                        | Default     |
+| --------- | --------- | ----------- | --------------------------------------------------------------------------- | ----------- |
+| `dark`    | `dark`    |             | `boolean`                                                                   | `false`     |
+| `striped` | `striped` |             | `boolean`                                                                   | `false`     |
+| `type`    | `type`    |             | `"danger" \| "muted" \| "primary" \| "secondary" \| "success" \| "warning"` | `'primary'` |
+| `value`   | `value`   |             | `number`                                                                    | `0`         |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
