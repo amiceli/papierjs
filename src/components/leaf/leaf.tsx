0fb0eff21113b6209d8306efebee2f345f804e34
amiceli <antoine@miceli.click>
2025-02-11 22:50:30 +0100

    feat: add p-leaf component (#5)

---
+++ src/components/leaf/leaf.tsx
@@ -0,0 +1,22 @@
+import { Component, Prop, h } from '@stencil/core'
+
+@Component({
+    tag: 'p-leaf',
+    styleUrl: 'leaf.scss',
+})
+export class Alert {
+    @Prop()
+    dark?: boolean = false
+
+    public getParentClass() {
+        return this.dark ? 'papier is--dark' : 'papier'
+    }
+
+    render() {
+        return (
+            <div class={this.getParentClass()}>
+                <slot />
+            </div>
+        )
+    }
+}
---
+++ src/components/leaf/readme.md
@@ -0,0 +1,17 @@
+# p-leaf
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description | Type      | Default |
+| -------- | --------- | ----------- | --------- | ------- |
+| `dark`   | `dark`    |             | `boolean` | `false` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
