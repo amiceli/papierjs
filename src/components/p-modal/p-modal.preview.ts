f99d4c844f41cec653f6abc4303f6b9cd3251855
amiceli <antoine@miceli.click>
2025-02-13 23:04:48 +0100

    feat: add modal component

---
+++ src/components/p-modal/p-modal.preview.ts
@@ -0,0 +1,32 @@
+import type { PModal } from '../../../dist/types/components/p-modal/p-modal'
+
+export function previewModal() {
+    const inputObserver = new MutationObserver(() => {
+        const modal = document.querySelector('#modal-1') as unknown as PModal
+        const buttonModal = document.querySelector('#open-modal-1')
+
+        const otherModal = document.querySelector(
+            '#modal-2',
+        ) as unknown as PModal
+        const otherButton = document.querySelector('#open-modal-2')
+        const closeIt = document.querySelector('#close-it')
+
+        if (modal && buttonModal && otherModal && otherButton) {
+            buttonModal.addEventListener('click', () => {
+                modal.open()
+            })
+            otherButton.addEventListener('click', () => {
+                otherModal.open()
+            })
+            closeIt.addEventListener('click', () => {
+                otherModal.close()
+            })
+            inputObserver.disconnect()
+        }
+    })
+
+    inputObserver.observe(document.body, {
+        childList: true,
+        subtree: true,
+    })
+}
