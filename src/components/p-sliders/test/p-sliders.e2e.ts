af71281d688c058d40f005f3034505af08d1ce95
amiceli <antoine@miceli.click>
2025-02-15 19:22:24 +0100

    test: min and max props for p-slider

--- src/components/p-sliders/test/p-sliders.e2e.ts
+++ src/components/p-sliders/test/p-sliders.e2e.ts
@@ -1,4 +1,4 @@
-import { newE2EPage } from '@stencil/core/testing'
+import { E2EElement, newE2EPage } from '@stencil/core/testing'
 
 describe('p-sliders', () => {
     it('renders', async () => {
@@ -7,5 +7,18 @@ describe('p-sliders', () => {
 
         const element = await page.find('p-sliders')
         expect(element).toHaveClass('hydrated')
+    })
+    it('define input min and max', async () => {
+        const page = await newE2EPage()
+        await page.setContent(`
+            <p-sliders 
+                min="10" 
+                max="100" 
+            ></p-sliders>
+        `)
+        const input = await page.find('p-sliders >>> input')
+
+        expect(input.getAttribute('max')).toEqual('100')
+        expect(input.getAttribute('min')).toEqual('10')
     })
 })
