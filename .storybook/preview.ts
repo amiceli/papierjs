b3fefd6f4a283790cc799e41eb0122221539d846
amiceli <antoine@miceli.click>
2025-02-15 21:48:58 +0100

    fix: some imports path

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/html'
 import { defineCustomElements } from '../loader'
 import { previewCloseAlert } from '../src/components/alert/stories/alert.preview'
-import { handleDropdownOnChange } from '../src/components/dropdown/dropdown.preview'
+import { handleDropdownOnChange } from '../src/components/dropdown/dropdown/dropdown.preview'
 import { handleInputCahnge } from '../src/components/form/input-text/stories/input-text.preview'
 import { previewSlider } from '../src/components/form/slider/sliders.preview'
 import { previewModal } from '../src/components/modal/modal.preview'
