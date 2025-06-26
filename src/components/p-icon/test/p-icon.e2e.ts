926a8984f31644aac6a8a85724eaa7402fbf73d5
amiceli <antoine@miceli.click>
2025-06-26 23:38:55 +0200

    test: p-icon component

--- src/components/p-icon/test/p-icon.e2e.ts
+++ src/components/p-icon/test/p-icon.e2e.ts
@@ -1,11 +1,39 @@
-import { newE2EPage } from '@stencil/core/testing'
+import {
+    type E2EElement,
+    type E2EPage,
+    newE2EPage,
+} from '@stencil/core/testing'
+import { icons } from '../icons'
 
 describe('p-icon', () => {
-    it('renders', async () => {
-        const page = await newE2EPage()
-        await page.setContent('<p-icon></p-icon>')
+    let page: E2EPage
+    let element: E2EElement
 
-        const element = await page.find('p-icon')
+    beforeEach(async () => {
+        page = await newE2EPage()
+        await page.setContent(
+            '<p-icon icon="user" color="red" size="100"></p-icon>',
+        )
+
+        element = await page.find('p-icon')
+    })
+
+    it('renders', async () => {
         expect(element).toHaveClass('hydrated')
+    })
+
+    it('should include svg', async () => {
+        const normalize = document.createElement('div')
+        normalize.innerHTML = icons.user
+
+        const div = element.shadowRoot.querySelector('div')
+        expect(div.innerHTML).toContain(normalize.innerHTML)
+    })
+
+    it('should apply styles', () => {
+        const div = element.shadowRoot.querySelector('div')
+
+        expect(div.style.color).toEqual('red')
+        expect(div.style.width).toEqual('100px')
     })
 })
