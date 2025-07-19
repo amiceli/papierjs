96c7988383445dc8cd70677e63a055b0aa22575c
amiceli <antoine@miceli.click>
2025-07-19 10:49:56 +0200

    chore: exclude *.stories.js so that Stencil ignores them at build

--- stencil.config.ts
+++ stencil.config.ts
@@ -11,6 +11,7 @@ export const config: Config = {
         }),
         inlineSvg(),
     ],
+    tsconfig: 'tsconfig.stencil.json',
     namespace: 'papierjs',
     extras: {
         enableImportInjection: true,
