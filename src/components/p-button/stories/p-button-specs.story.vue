8f5aa7c892085f57f5b0671cf50af644316db8af
amiceli <antoine@miceli.click>
2026-02-12 22:28:20 +0100

    refactor: use Histoire for p-button stories

---
+++ src/components/p-button/stories/p-button-specs.story.vue
@@ -0,0 +1,23 @@
+<template>
+    <Story title="p-button/Specs">
+        <div class="markstream-vue">
+
+            <MarkdownRender
+                :content="readme"
+                :isDark="true"
+            />
+        </div>
+    </Story>
+</template>
+
+<script lang="ts" setup>
+import MarkdownRender from 'markstream-vue'
+import readme from '../readme.md?raw'
+import 'markstream-vue/index.css'
+</script>
+
+<style scoped>
+.markstream-vue {
+    color: white;
+}
+</style>
---
+++ src/components/p-button/stories/p-button.story.vue
@@ -0,0 +1,108 @@
+<template>
+    <Story
+        :layout="{ type: 'grid', width: '90%' }"
+        title="p-button/Overview"
+    >
+        <Variant title="Default">
+            <p-button
+                :dark="state.darkMode"
+                v-for="b in buttons"
+                :key="`btn-${b}`"
+                :type="b"
+                :disabled="state.disabled"
+            >
+                {{ b }}
+            </p-button>
+        </Variant>
+        <Variant title="Outline">
+            <p-button
+                :dark="state.darkMode"
+                v-for="b in buttons"
+                :type="b"
+                outline
+                :key="`out-btn-${b}`"
+                :disabled="state.disabled"
+            >
+                {{ b }}
+            </p-button>
+        </Variant>
+        <Variant title="Size">
+            <p-button
+                :dark="state.darkMode"
+                type="success"
+                :disabled="state.disabled"
+                large
+            >
+                Large
+            </p-button>
+            <p-button
+                :dark="state.darkMode"
+                type="success"
+                :disabled="state.disabled"
+                small
+            >
+                small
+            </p-button>
+            <br>
+            <div style="margin-top: 20px;; width : 400px">
+                <p-button
+                    :dark="state.darkMode"
+                    type="danger"
+                    :disabled="state.disabled"
+                    block
+                >
+                    block
+                </p-button>
+            </div>
+        </Variant>
+        <Variant title="Loading">
+            <p-button
+                :dark="state.darkMode"
+                v-for="b in buttons"
+                :type="b"
+                loading
+                :key="`load-btn-${b}`"
+            >
+                {{ b }}
+            </p-button>
+        </Variant>
+
+        <template #controls>
+            <HstCheckbox
+                v-model="state.darkMode"
+                title="Dark mode"
+            />
+            <HstCheckbox
+                v-model="state.disabled"
+                title="Disabled"
+            />
+        </template>
+    </Story>
+</template>
+
+<docs lang="md"># My documentation
+
+Checkout this [cool video](https://www.youtube.com/watch?v=dQw4w9WgXcQ)!</docs>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+const state = reactive({
+    darkMode: false,
+    disabled: false,
+})
+
+const buttons = [
+    'success',
+    'secondary',
+    'primary',
+    'danger',
+    'warning',
+]
+</script>
+
+<style lang="scss" scoped>
+p-button {
+    margin-right: 10px;
+}
+</style>
