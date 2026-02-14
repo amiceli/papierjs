49813bf9b02a378a41afac2139c3f7bdbbee6856
amiceli <antoine@miceli.click>
2026-02-14 12:53:39 +0100

    feat: add stories for dropdown

---
+++ src/components/breadcrumb/p-breadcrumb/stories/p-breadcrumb.story.vue
@@ -0,0 +1,43 @@
+<template>
+    <Story
+        :layout="{ type: 'grid', width: '90%' }"
+        title="Components/Breadcrumb/Overview"
+    >
+        <Variant title="Overview">
+            <p-breadcrumb :dark="darkMode">
+                <p-breadcrumb-item link="#">
+                    <span>Histoire</span>
+                </p-breadcrumb-item>
+                <p-breadcrumb-item link="#">
+                    <span>Livre</span>
+                </p-breadcrumb-item>
+                <p-breadcrumb-item>
+                    <span>Chapitre</span>
+                </p-breadcrumb-item>
+            </p-breadcrumb>
+        </Variant>
+
+        <Variant title="One link">
+            <p-breadcrumb :dark="darkMode">
+                <p-breadcrumb-item link="#">
+                    <span>Maison</span>
+                </p-breadcrumb-item>
+            </p-breadcrumb>
+        </Variant>
+
+        <template #controls>
+            <HstCheckbox
+                v-model="darkMode"
+                title="Dark mode"
+            />
+        </template>
+    </Story>
+</template>
+
+<script lang="ts" setup>
+import { useDarkMode } from '@h/useDarkMode'
+
+const { darkMode } = useDarkMode()
+</script>
+
+<style lang="scss" scoped></style>
