00a1d25e8830742c47aa29b3d22257dfce9bed2a
amiceli <antoine@miceli.click>
2026-02-14 14:24:55 +0100

    feat: add stories for switch components

---
+++ src/components/form/p-switch-tile/stories/p-switch-tile.story.vue
@@ -0,0 +1,105 @@
+<template>
+    <Story
+        :layout="{ type: 'grid', width: '90%' }"
+        title="Form/Switch Tile/Overview"
+    >
+        <Variant title="Sample">
+            <p-switch-tile
+                :dark="darkMode"
+                :checked="state.checked"
+            >
+                <span slot="checked-text">
+                    {{ state.checkedText }}
+                </span>
+                <span slot="unchecked-text">
+                    {{ state.uncheckedText }}
+                </span>
+            </p-switch-tile>
+        </Variant>
+
+        <Variant title="All colors">
+            <p-switch-tile
+                checked-background="warning"
+                :checked="state.checked"
+                :dark="darkMode"
+            >
+                <span slot="checked-text">warning</span>
+                <span slot="unchecked-text">default</span>
+            </p-switch-tile>
+
+            <p-switch-tile
+                checked-background="primary"
+                :checked="state.checked"
+                :dark="darkMode"
+            >
+                <span slot="checked-text">primary</span>
+                <span slot="unchecked-text">default</span>
+            </p-switch-tile>
+
+            <p-switch-tile
+                checked-background="secondary"
+                :checked="state.checked"
+                :dark="darkMode"
+            >
+                <span slot="checked-text">secondary</span>
+                <span slot="unchecked-text">default</span>
+            </p-switch-tile>
+
+            <p-switch-tile
+                checked-background="danger"
+                :checked="state.checked"
+                :dark="darkMode"
+            >
+                <span slot="checked-text">danger</span>
+                <span slot="unchecked-text">default</span>
+            </p-switch-tile>
+
+            <p-switch-tile
+                checked-background="noted"
+                :checked="state.checked"
+                :dark="darkMode"
+            >
+                <span slot="checked-text">noted</span>
+                <span slot="unchecked-text">default</span>
+            </p-switch-tile>
+        </Variant>
+
+        <template #controls>
+            <HstCheckbox
+                v-model="darkMode"
+                title="Dark mode"
+            />
+            <HstCheckbox
+                v-model="state.checked"
+                title="Checked"
+            />
+            <HstText
+                v-model="state.checkedText"
+                title="Checked slot text"
+            />
+            <HstText
+                v-model="state.uncheckedText"
+                title="Unchecked slot text"
+            />
+        </template>
+    </Story>
+</template>
+
+<script lang="ts" setup>
+import { useDarkMode } from '@h/useDarkMode'
+import { reactive } from 'vue'
+
+const { darkMode } = useDarkMode()
+
+const state = reactive({
+    checked: false,
+    checkedText: 'Yes',
+    uncheckedText: 'No',
+})
+</script>
+
+<style scoped>
+table th {
+    min-width: 200px;
+}
+</style>
---
+++ src/components/form/p-switch/stories/p-switch.story.vue
@@ -0,0 +1,87 @@
+<template>
+    <Story
+        :layout="{ type: 'grid', width: '90%' }"
+        title="Form/Switch/Overview"
+    >
+        <Variant title="Sample">
+            <p-switch
+                :dark="darkMode"
+                :checked="state.checked"
+            ></p-switch>
+        </Variant>
+
+        <Variant title="Styles">
+            <p-leaf>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                round
+                            </th>
+                            <th>
+                                square
+                            </th>
+                            <th>
+                                round & square
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p-switch
+                                    :dark="darkMode"
+                                    :checked="state.checked"
+                                    round
+                                ></p-switch>
+                            </td>
+                            <td>
+                                <p-switch
+                                    :dark="darkMode"
+                                    :checked="state.checked"
+                                    square
+                                ></p-switch>
+                            </td>
+                            <td>
+                                <p-switch
+                                    :dark="darkMode"
+                                    :checked="state.checked"
+                                    square
+                                    round
+                                ></p-switch>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </p-leaf>
+        </Variant>
+
+        <template #controls>
+            <HstCheckbox
+                v-model="darkMode"
+                title="Dark mode"
+            />
+            <HstCheckbox
+                v-model="state.checked"
+                title="Checked"
+            />
+        </template>
+    </Story>
+</template>
+
+<script lang="ts" setup>
+import { useDarkMode } from '@h/useDarkMode'
+import { reactive } from 'vue'
+
+const { darkMode } = useDarkMode()
+
+const state = reactive({
+    checked: false,
+})
+</script>
+
+<style scoped>
+table th {
+    min-width: 200px;
+}
+</style>
