ff058ce9bd46d3be95843a1ebe6840b09d800dfc
amiceli <antoine@miceli.click>
2026-02-14 14:04:19 +0100

    feat: add stories for slider component

---
+++ src/components/form/p-slider/stories/p-slider.story.vue
@@ -0,0 +1,66 @@
+<template>
+    <Story
+        :layout="{ type: 'grid', width: '90%' }"
+        title="Form/Slider/Overview"
+    >
+        <Variant title="Sample">
+            <p-slider
+                :dark="darkMode"
+                :value="state.value"
+                @change="state.value = $event.detail"
+                :min="state.min"
+                :max="state.max"
+            ></p-slider>
+        </Variant>
+
+        <Variant title="Size">
+            <p-slider
+                :dark="darkMode"
+                :value="state.value"
+                @change="state.value = $event.detail"
+                :min="state.min"
+                :max="state.max"
+                block
+            ></p-slider>
+        </Variant>
+
+        <template #controls>
+            <HstCheckbox
+                v-model="darkMode"
+                title="Dark mode"
+            />
+            <HstNumber
+                v-model="state.min"
+                :step="1"
+                title="Min"
+                :max="state.max"
+            />
+            <HstNumber
+                v-model="state.max"
+                :step="1"
+                :min="state.min"
+                title="max"
+            />
+            <HstNumber
+                v-model="state.value"
+                :step="1"
+                :min="state.min"
+                :max="state.max"
+                title="Slider value"
+            />
+        </template>
+    </Story>
+</template>
+
+<script lang="ts" setup>
+import { useDarkMode } from '@h/useDarkMode'
+import { reactive } from 'vue'
+
+const { darkMode } = useDarkMode()
+
+const state = reactive({
+    min: 0,
+    max: 100,
+    value: 69,
+})
+</script>
