a8f9e4025f56dd56306250603e253bf7a7cd2672
amiceli <antoine@miceli.click>
2026-03-31 10:51:28 +0200

    fix: use svgo to optimize svg

--- scripts/copy-svg.js
+++ scripts/copy-svg.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs')
 const path = require('node:path')
+const { optimize } = require('svgo')
 
 const inputDir = 'node_modules/pixelarticons/svg'
 const output = 'src/components/p-icon/icons.ts'
@@ -8,12 +9,21 @@ const files = fs.readdirSync(inputDir).filter((f) => f.endsWith('.svg'))
 
 const entries = files.map((f) => {
     const name = path.basename(f, '.svg')
-    const content = fs
-        .readFileSync(path.join(inputDir, f), 'utf8')
+    const content = fs.readFileSync(path.join(inputDir, f), 'utf8')
+
+    const optimized = optimize(content.toString(), {
+        plugins: [
+            'removeViewBox',
+            'removeDimensions',
+        ],
+    })
+
+    const end = optimized.data
         .replace(/\r?\n|\t/g, ' ')
         .replace(/"/g, `'`)
         .replace(/>[\s]+</g, '><')
-    return `  "${name}": "${content}"`
+
+    return `  "${name}": "${end}"`
 })
 
 fs.writeFileSync(output, `export const icons: Record<string, string> = {\n${entries.join(',\n')}\n}\n`)
